86f81735666c58534c8ddbc0008c82328e23da0d
Divya <chacherkardivya@gmail.com>
2026-09-16 00:07:07 -0700

    Updated day

---
+++ LogicBuilding/Day03/threeNumberDistinct.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2, n3;
+    cout << "Enter First Number : ";
+    cin >> n1;
+    cout << "Enter Second Number : ";
+    cin >> n2;
+    cout << "Enter Third Number : ";
+    cin >> n3;
+
+    if (n1 != n2 && n1 != n3 && n2 != n3)
+    {
+        cout << "All the numbers are Distinct";
+    }
+    else
+    {
+        cout << "Numbers are same ";
+    }
+}
